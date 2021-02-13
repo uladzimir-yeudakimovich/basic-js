2c3e289a823fc3ca53761697adcfbf9c0d274ff2
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2021-02-13 20:08:17 +0300

    dream team done

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members || !members.length) return false;
+  return members.map(el => {
+    if (typeof el === 'string') {
+      return el.trim()[0].toUpperCase();
+    }
+  }).sort().join('');
 };
