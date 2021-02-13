75d4bbea3025b10c2b36d91451ed7b993008646c
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2021-02-13 22:46:11 +0300

    seasons done

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  if (!date.toDateString()) return 'Error';
+  const mounth = date.getMonth();
+  if (mounth >= 0 && mounth < 2 || mounth === 11) {
+    return 'winter';
+  } else if (mounth < 5) {
+    return 'spring';
+  } else if (mounth < 8) {
+    return 'summer';
+  } else {
+    return 'autumn';
+  }
 };
