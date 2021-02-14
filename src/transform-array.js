08206292f008816e9357fdc7b3485233f9a77964
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2021-02-14 16:50:24 +0300

    transrorm array without 1 test

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new CustomError(`${arr} is not Array`);
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      i += 2;
+    } else if (arr[i] === '--discard-prev') {
+      if (i > 1 && newArr.length) newArr.pop();
+    } else if (arr[i] === '--double-next') {
+      if (i < arr.length - 1) newArr.push(arr[i + 1]);
+    } else if (arr[i] === '--double-prev') {
+      if (i > 1) newArr.push(arr[i - 1]);
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 };
