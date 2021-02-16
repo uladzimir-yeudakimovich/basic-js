daaff3c220799913a9a03850f040543aed1a6e56
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2021-02-17 01:18:42 +0300

    refactor: crypt

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,32 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
   constructor(simple) {
     this.simple = simple || true;
   }
 
   encrypt(message, key) {
-    this.dataCheck(message, key);
-    message = message.toUpperCase();
-    const long = message.length;
-    const shifr = this.shifrCreator(key, long);
-    let text = '';
-    let j = 0;
-    for (let i = 0; i < long; i++) {
-      if (/[A-Za-z]/.test(message[i])) {
-        text += this.alphabet[(this.alphabet.indexOf(message[i]) + this.alphabet.indexOf(shifr[j])) % 26];
-        j += 1;
-      } else {
-        text += message[i];
-      }
-    }
-    return this.simple ? text : text.split('').reverse().join('');
+    return this.mainLogic(message, key, 'encrypt');
   }
 
   decrypt(message, key) {
-    this.dataCheck(message, key);
+    return this.mainLogic(message, key, 'decrypt');
+  }
+
+  mainLogic(message, key, action) {
+    if (!message || !key) throw `Error: message and key required`;
     message = message.toUpperCase();
     const long = message.length;
     const shifr = this.shifrCreator(key, long);
@@ -34,17 +23,18 @@ class VigenereCipheringMachine {
     let j = 0;
     for (let i = 0; i < long; i++) {
       if (/[A-Za-z]/.test(message[i])) {
-        text += this.alphabet[(26 + this.alphabet.indexOf(message[i]) - this.alphabet.indexOf(shifr[j])) % 26];
+        const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+        if (action === 'encrypt') {
+          text += alphabet[(alphabet.indexOf(message[i]) + alphabet.indexOf(shifr[j])) % 26];
+        } else if (action === 'decrypt') {
+          text += alphabet[(26 + alphabet.indexOf(message[i]) - alphabet.indexOf(shifr[j])) % 26];
+        }
         j += 1;
       } else {
         text += message[i];
       }
     }
     return this.simple ? text : text.split('').reverse().join('');
-  }
-
-  dataCheck(message, key) {
-    if (!message || !key) throw `Error: message and key required`;
   }
 
   shifrCreator(key, long) {
