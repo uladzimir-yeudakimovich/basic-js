22d4a61ca3d7c3467bb840918a79cfbb0dad0630
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2021-02-16 20:23:52 +0300

    fix: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -5,13 +5,13 @@ module.exports = function transform(arr) {
   const newArr = [];
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === '--discard-next') {
-      i += 2;
+      i += 1;
     } else if (arr[i] === '--discard-prev') {
-      if (i > 1 && newArr.length) newArr.pop();
+      if (i > 1 && newArr.length && arr[i - 2] !== '--discard-next') newArr.pop();
     } else if (arr[i] === '--double-next') {
       if (i < arr.length - 1) newArr.push(arr[i + 1]);
     } else if (arr[i] === '--double-prev') {
-      if (i > 1) newArr.push(arr[i - 1]);
+      if (i > 1 && arr[i - 2] !== '--discard-next') newArr.push(arr[i - 1]);
     } else {
       newArr.push(arr[i]);
     }
